dc2ec27fcec734695a8039d637fd8f9f7a248020
AmauriLima <amauri.plimaj@gmail.com>
2022-10-11 20:17:41 -0300

    feat: adds application design tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,16 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7C7C8A',
+      'gray-200': '#E1E1E6',
+      'gray-100': '#C4C4CC',
+
+      'cyan-500': '#81D8F7',
+      'cyan-300': '#9BE1FB'
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
