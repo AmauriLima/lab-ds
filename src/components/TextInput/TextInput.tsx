d2f296f317701689f16c844dd5239a617c6944bb
AmauriLima <amauri.plimaj@gmail.com>
2022-10-12 12:05:42 -0300

    feat: adds MountedTextInput component

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+
+import { Envelope, IconProps } from "phosphor-react";
+
 import { TextInputRoot } from "./components/TextInputRoot";
 import { TextInputIcon } from "./components/TextInputIcon";
 import { TextInputInput } from "./components/TextInputInput";
@@ -10,4 +14,25 @@ export const TextInput = {
   Root: TextInputRoot,
   Input: TextInputInput,
   Icon: TextInputIcon,
+};
+
+interface MountedTextInputProps {
+  Icon?: React.ForwardRefExoticComponent<
+    IconProps & React.RefAttributes<SVGSVGElement>
+  >;
+}
+
+export const MountedTextInput: React.FC<MountedTextInputProps> = (props) => {
+  const { Icon } = props;
+
+  return (
+    <TextInput.Root>
+      {Icon && (
+        <TextInput.Icon>
+          <Icon />
+        </TextInput.Icon>
+      )}
+      <TextInput.Input placeholder="Type yout e-mail address" />,
+    </TextInput.Root>
+  );
 };
